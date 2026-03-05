dd2908955b3008759312c43ce009ef0c7453c7fc
Javid Ahmed <57677851+javidahmed64592@users.noreply.github.com>
2026-03-05 21:22:37 +0000

    Refactor Python dependency installation actions and update workflows (#21)
    * Refactor Python dependency installation actions and update workflows
    
    * Update action to install Python core dependencies in build workflow
    
    * Move to subdirectory
    
    * Refactor action paths to use the setup directory for Python installation in CI and build workflows
    
    * Standardize action names for Python dependency installation and setup
    
    * Add CI and build actions for Python package management and validation
    
    * Implement version checking utility for CI workflows and update dependencies
    
    * Refactor version retrieval functions and add unit tests for workflows
    
    * Add Python version setup to uv + Python action
    
    * Fix mypy
    
    * Enhance WORKFLOWS.md with detailed descriptions of reusable actions and CI actions
    
    * Remove note about required scripts for version checks in WORKFLOWS.md

---
+++ template_python/workflows.py
@@ -0,0 +1,49 @@
+"""Utility script for GitHub workflows."""
+
+import tomllib
+from functools import cache
+
+from pyhere import here
+
+
+@cache
+def _load_pyproject() -> dict:
+    """Load the pyproject.toml file."""
+    with here("pyproject.toml").open("rb") as f:
+        return tomllib.load(f)
+
+
+@cache
+def _load_uv_lock() -> dict:
+    """Load the uv.lock file."""
+    with here("uv.lock").open("rb") as f:
+        return tomllib.load(f)
+
+
+@cache
+def _get_version_pyproject() -> str:
+    """Get the version from pyproject.toml."""
+    pyproject = _load_pyproject()
+    return str(pyproject["project"]["version"])
+
+
+@cache
+def _get_version_uv_lock() -> str:
+    """Get the version from uv.lock."""
+    pyproject = _load_pyproject()
+    uv_lock = _load_uv_lock()
+    if pkg := next((p for p in uv_lock["package"] if p["name"] == pyproject["project"]["name"]), None):
+        return str(pkg["version"])
+
+    error_msg = f"Package '{pyproject['project']['name']}' not found in uv.lock"
+    raise ValueError(error_msg)
+
+
+def print_version_pyproject() -> None:
+    """Get the version from pyproject.toml."""
+    print(_get_version_pyproject())
+
+
+def print_version_uv_lock() -> None:
+    """Get the version from uv.lock."""
+    print(_get_version_uv_lock())
