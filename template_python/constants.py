cc86cfbe66cb486174e44f46f10ed555b9ac007c
Javid Ahmed <javidahmed@icloud.com>
2026-04-12 20:50:07 +0100

    Refactor Sphinx configuration by moving extensions and theme to constants; add RST prolog generator function and corresponding tests

---
+++ template_python/constants.py
@@ -0,0 +1,12 @@
+"""Constants for codebases using this template."""
+
+# Sphinx
+SPHINX_EXTENSIONS = [
+    "sphinx.ext.autodoc",  # Auto-generate API docs from docstrings
+    "sphinx.ext.autosummary",  # Generate summary tables
+    "sphinx.ext.napoleon",  # Support Google/NumPy style docstrings
+    "sphinx.ext.viewcode",  # Add links to highlighted source code
+    "sphinx.ext.intersphinx",  # Link to other project documentation
+    "sphinx_autodoc_typehints",  # Better type hint rendering
+]
+SPHINX_HTML_THEME = "furo"
